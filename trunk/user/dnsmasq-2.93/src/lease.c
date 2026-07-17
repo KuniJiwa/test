5a5714dde03167a06fed523ef89d9ff2225535e8
KuniJiwa <45271734+KuniJiwa@users.noreply.github.com>
2026-07-18 02:37:50 +0800

    Update lease.c

--- trunk/user/dnsmasq-2.93/src/lease.c
+++ trunk/user/dnsmasq-2.93/src/lease.c
@@ -1064,9 +1064,12 @@ void lease_set_hostname(struct dhcp_lease *lease, const char *name, int auth, ch
   struct dhcp_lease *lease_tmp;
   char *new_name = NULL, *new_fqdn = NULL;
 
+#if 0
+/* disable warning, noisy */
   if (config_domain && (!domain || !hostname_isequal(domain, config_domain)))
     my_syslog(MS_DHCP | LOG_WARNING, _("Ignoring domain %s for DHCP host name %s"), config_domain, name);
-  
+#endif
+	
   if (lease->hostname && name && hostname_isequal(lease->hostname, name))
     {
       if (auth)
