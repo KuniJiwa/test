8dc5424e291fa04aa8a4b3debf55e0c6195e6c57
KuniJiwa <45271734+KuniJiwa@users.noreply.github.com>
2026-07-18 05:05:44 +0800

    Update ipset.c

--- trunk/user/dnsmasq-2.93/src/ipset.c
+++ trunk/user/dnsmasq-2.93/src/ipset.c
@@ -99,7 +99,11 @@ static int new_add_to_ipset(const char *setname, const union all_addr *ipaddr, i
   struct my_nfgenmsg *nfg;
   struct my_nlattr *nested[2];
   uint8_t proto;
+#ifdef HAVE_IPV6
   int addrsz = (af == AF_INET6) ? IN6ADDRSZ : INADDRSZ;
+#else
+  int addrsz = INADDRSZ;
+#endif /* HAVE_IPV6 */
 
   if (strlen(setname) >= IPSET_MAXNAMELEN) 
     {
@@ -145,8 +149,10 @@ int add_to_ipset(const char *setname, const union all_addr *ipaddr, int flags, i
 {
   int ret = 0, af = AF_INET;
 
+#ifdef HAVE_IPV6
   if (flags & F_IPV6)
       af = AF_INET6;
+#endif /* HAVE_IPV6 */
 
   ret = new_add_to_ipset(setname, ipaddr, af, remove);
 
